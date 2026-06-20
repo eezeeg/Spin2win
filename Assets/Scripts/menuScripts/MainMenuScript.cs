ee26f1b31796314a24627a69f14c8535617db79d
eezeeg <akcekayaismail1@gmail.com>
2026-06-20 15:53:22 +0200

    level select: button disable for locked levels.

--- Assets/Scripts/menuScripts/MainMenuScript.cs
+++ Assets/Scripts/menuScripts/MainMenuScript.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] private GameObject panelMain;
     [SerializeField] private GameObject panelSelect;
+    [SerializeField] private List<GameObject> levelList;
     public void PlayGame()
     {
         panelMain.SetActive(false);
@@ -21,5 +27,54 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         panelMain.SetActive(true);
         panelSelect.SetActive(false);
+    }
+
+    private void Start()
+    {
+        int totalComplete = 0;
+        int levelId = 1;
+        foreach (GameObject level in levelList)
+        {
+            if (LevelsBeatSave.IsLevelComplete(levelId))
+            {
+                totalComplete++;
+                TMP_Text timer = level.transform.Find("CompleteTime").GetComponent<TMP_Text>();
+                float time = LevelsBeatSave.GetBestTime(levelId);
+                timer.text = FormatTime(time);
+            }
+            if (levelId > 3)
+            {
+                
+                //disable if not enough levels complete
+                if (totalComplete + 3 < levelId)
+                {
+                    GameObject buttonObject = level.transform.Find("BtnLvl").gameObject;
+                    Button button = buttonObject.GetComponent<Button>();
+                    button.interactable = false;
+
+                    TMP_Text btnText = button.transform.Find("text").GetComponent<TMP_Text>();
+                    btnText.text = "locked";
+
+                }
+            }
+
+            levelId++;
+        }
+
+
+    }
+
+
+
+    //helper for timer format
+    private string FormatTime(float time)
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        int milliseconds = Mathf.FloorToInt((time * 100) % 100f);
+
+        return minutes.ToString("00") + ":" + 
+               seconds.ToString("00") + "." + 
+               milliseconds.ToString("00");
     }
 }
