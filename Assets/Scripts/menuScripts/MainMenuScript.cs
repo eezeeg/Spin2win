d2dd0936a645321f70d67b64822146c77cc2b9ba
eezeeg <akcekayaismail1@gmail.com>
2026-06-23 08:07:48 +0200

    create acc panels, still some issues
    scale tie,
    button ugly,
    canvas ugly

--- Assets/Scripts/menuScripts/MainMenuScript.cs
+++ Assets/Scripts/menuScripts/MainMenuScript.cs
@@ -2,12 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public static bool isLevelSelect;
     public static int selectedPage = 0;
+
+    [Header("Testing")]
+    public GameObject marble;
+    public GameObject tieMarble;
+
+    [Header("Accessories Canvas")]
+    [SerializeField] private RectTransform accessoriesCanvas;
+    [SerializeField] private GameObject panel1;
+    [SerializeField] private GameObject panel2;
+    [SerializeField] private GameObject panel3;
+    [Header("Prefabs accesoires")]
+    [SerializeField] private List<Material> MatList1;
+    [SerializeField] private List<Material> MatList2;
+    [SerializeField] private List<GameObject> Shape1List;
+
+    [SerializeField] private float accessoriesCanvasMoveDur = 1f;
+    [SerializeField] private float accessoriesHiddenX = -1920f;
+    [SerializeField] private float accessoriesShownX = 0f;
+    private Coroutine accessoriesCanvasCoroutine;
+    [Header("button prefab")]
+    [SerializeField] private Button buttonPrefab; 
 
     [Header("Second Camera")]
     [SerializeField] private Camera SecondCam;
@@ -33,6 +56,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void Awake()
     {
+        Vector2 pos = accessoriesCanvas.anchoredPosition;
+        pos.x = accessoriesHiddenX;
+        accessoriesCanvas.anchoredPosition = pos;
+        CreateAccButtons();
         if (isLevelSelect)
         {
             PlayGame();
@@ -59,14 +86,172 @@ public class NewMonoBehaviourScript : MonoBehaviour
         isLevelSelect = true;
     }
 
-    public void GoAccesories()
+    private void CreateAccButtons()
+    {
+        CreateMaterial1Buttons();
+        CreateMaterial2Buttons();
+        CreatShapeButton();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(panel1.GetComponent<RectTransform>());
+        LayoutRebuilder.ForceRebuildLayoutImmediate(panel2.GetComponent<RectTransform>());
+        LayoutRebuilder.ForceRebuildLayoutImmediate(panel3.GetComponent<RectTransform>());
+    }
+
+    private void CreateMaterial1Buttons()
+    {
+        Debug.Log("Creating material buttons. Count: " + MatList1.Count);
+        foreach (Material material in MatList1)
+        {
+            Debug.Log("material: " + material.name);
+            Button newButton = Instantiate(buttonPrefab, panel1.transform);
+            newButton.gameObject.SetActive(true);
+            newButton.name = material.name;
+
+            TMP_Text buttonText = newButton.GetComponentInChildren<TMP_Text>();
+            if (buttonText != null)
+            {
+                buttonText.text = material.name;
+            }
+            Material currentMaterial = material;
+
+            newButton.onClick.AddListener(() =>
+            {
+                SelectMaterial(currentMaterial);
+            });
+        }
+    }
+
+    private void CreateMaterial2Buttons()
+    {
+        Debug.Log("Creating material 2 buttons. Count: " + MatList2.Count);
+
+        foreach (Material material in MatList2)
+        {
+            Debug.Log("material 2: " + material.name);
+
+            Button newButton = Instantiate(buttonPrefab, panel3.transform);
+            newButton.gameObject.SetActive(true);
+            newButton.name = material.name;
+
+            TMP_Text buttonText = newButton.GetComponentInChildren<TMP_Text>(true);
+            if (buttonText != null)
+            {
+                buttonText.text = material.name;
+            }
+
+            Material currentMaterial = material;
+
+            newButton.onClick.AddListener(() =>
+            {
+                SelectMaterial2(currentMaterial);
+            });
+        }
+    }
+
+    private void CreatShapeButton()
+    {
+        Debug.Log("Creating shape buttons. Count: " + Shape1List.Count);
+
+        foreach (GameObject shape in Shape1List)
+        {
+            Debug.Log("shape: " + shape.name);
+
+            Button newButton = Instantiate(buttonPrefab, panel2.transform);
+            newButton.gameObject.SetActive(true);
+            newButton.name = shape.name;
+
+            TMP_Text buttonText = newButton.GetComponentInChildren<TMP_Text>(true);
+            if (buttonText != null)
+            {
+                buttonText.text = shape.name;
+            }
+
+            GameObject currentShape = shape;
+
+            newButton.onClick.AddListener(() =>
+            {
+                SelectShape(currentShape);
+            });
+        }
+    }
+    private void SelectMaterial(Material material)
+    {
+        Renderer renderer = marble.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = material;
+        }
+    }
+    private void SelectMaterial2(Material material)
+    {
+        Renderer renderer = tieMarble.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = material;
+        }
+    }
+    private void SelectShape(GameObject shape)
+    {
+        MeshFilter marbleMeshFilter = tieMarble.GetComponent<MeshFilter>();
+        MeshFilter shapeMeshFilter = shape.GetComponentInChildren<MeshFilter>();
+
+        if (marbleMeshFilter == null)
+        {
+            return;
+        }
+
+        if (shapeMeshFilter == null)
+        {
+            return;
+        }
+
+        marbleMeshFilter.mesh = shapeMeshFilter.sharedMesh;
+
+        tieMarble.transform.localScale = shape.transform.localScale;
+    }
+    public void showAccPanel(int panelInt)
+    {
+        switch (panelInt)
+        {
+            case 1:
+                panel1.SetActive(true);
+                panel2.SetActive(false);
+                panel3.SetActive(false);
+                break;
+            case 2:
+                panel1.SetActive(false);
+                panel2.SetActive(true);
+                panel3.SetActive(false);
+                break;
+            case 3:
+                panel1.SetActive(false);
+                panel2.SetActive(false);
+                panel3.SetActive(true);
+                break;
+        }
+    }
+    public void GoAccesories(bool isGoing)
     {
         if (viewportCoroutine != null)
         {
             StopCoroutine(viewportCoroutine);
         }
 
-        viewportCoroutine = StartCoroutine(LerpCameraViewport(SecondCam, 0f));
+        if (accessoriesCanvasCoroutine != null)
+        {
+            StopCoroutine(accessoriesCanvasCoroutine);
+        }
+
+
+        if (isGoing)
+        {
+            viewportCoroutine = StartCoroutine(LerpCameraViewport(SecondCam, 0f)); 
+            accessoriesCanvasCoroutine = StartCoroutine(LerpCanvas(accessoriesCanvas, accessoriesShownX));
+        }
+        else
+        {
+            viewportCoroutine = StartCoroutine(LerpCameraViewport(SecondCam, 0.3f)); 
+            accessoriesCanvasCoroutine = StartCoroutine(LerpCanvas(accessoriesCanvas, accessoriesHiddenX));
+        }
     }
 
     private IEnumerator LerpCameraViewport(Camera cam, float targetx)
@@ -93,6 +278,27 @@ public class NewMonoBehaviourScript : MonoBehaviour
         viewportCoroutine = null;
     }
 
+    private IEnumerator LerpCanvas(RectTransform canvasRect, float targetX)
+    {
+        Vector2 startPos = canvasRect.anchoredPosition;
+        Vector2 targetPos = startPos;
+        targetPos.x = targetX;
+
+        float elapsed = 0f;
+        while (elapsed < accessoriesCanvasMoveDur)
+        {
+            elapsed += Time.deltaTime;
+
+            float time = elapsed / accessoriesCanvasMoveDur;
+            time = Mathf.SmoothStep(0f, 1f, time);
+
+            canvasRect.anchoredPosition = Vector2.Lerp(startPos, targetPos, time);
+
+            yield return null;
+        }
+        canvasRect.anchoredPosition = targetPos;
+        accessoriesCanvasCoroutine = null;
+    }
     public void QuitGame()
     {
         Application.Quit();
